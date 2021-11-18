6ee4452c910201a4a33728c832aa16925e059095
Omer Siton <omersiton11@gmail.com>
2021-11-18 17:04:52 +0200

    project 7 - initial commit

---
+++ 08/CodeWriter.py
@@ -0,0 +1,290 @@
+"""This file is part of nand2tetris, as taught in The Hebrew University,
+and was written by Aviv Yaish according to the specifications given in  
+https://www.nand2tetris.org (Shimon Schocken and Noam Nisan, 2017)
+and as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0 
+Unported License (https://creativecommons.org/licenses/by-nc-sa/3.0/).
+"""
+import typing
+
+
+class CodeWriter:
+    """Translates VM commands into Hack assembly code."""
+    segment_dic = {"local": "LCL", "argument": "ARG", "this": "THIS",
+                   "that": "THAT", "temp": 5, "pointer": 3}
+    push_SP = "@SP\n" \
+              "AM=M+1\n" \
+              "A=A-1\n" \
+              "M=D\n"
+    pop_SP = "@SP\n" \
+             "AM=M-1\n" \
+             "D=M\n" \
+             "M=0\n"
+    END_LOOP = "(END)\n" \
+               "@END\n" \
+               "0;JMP\n"
+
+    def __init__(self, output_stream: typing.TextIO) -> None:
+        """Initializes the CodeWriter.
+
+        Args:
+            output_stream (typing.TextIO): output stream.
+        """
+        self.output_stream = output_stream
+        self.file_name = ""
+        self.label_counter = 0
+
+    def set_file_name(self, filename: str) -> None:
+        """Informs the code writer that the translation of a new VM file is 
+        started.
+
+        Args:
+            filename (str): The name of the VM file.
+        """
+        # Your code goes here!
+        self.file_name = filename
+
+    def write_arithmetic(self, command: str) -> None:
+        """Writes the assembly code that is the translation of the given 
+        arithmetic command.
+
+        Args:
+            command (str): an arithmetic command.
+        """
+        # Your code goes here!
+        arithmetic_functions = {"add": self.add_sub, "sub": self.add_sub,
+                                "eq": self.eq,
+                                "gt": self.gt,
+                                "lt": self.lt,
+                                "and": self.and_or, "or": self.and_or,
+                                "not": self.neg_not_shift,
+                                "neg": self.neg_not_shift,
+                                "shiftleft": self.neg_not_shift,
+                                "shiftright": self.neg_not_shift
+                                }
+        self.output_stream.write(arithmetic_functions[command](command))
+
+    def write_push_pop(self, command: str, segment: str, index: int) -> None:
+        """Writes the assembly code that is the translation of the given 
+        command, where command is either C_PUSH or C_POP.
+
+        Args:
+            command (str): "C_PUSH" or "C_POP".
+            segment (str): the memory segment to operate on.
+            index (int): the index in the memory segment.
+        """
+        # Your code goes here!
+        if command == "C_PUSH":
+            push_functions = {"local": self.push_lcl_arg_this_that,
+                              "argument": self.push_lcl_arg_this_that,
+                              "this": self.push_lcl_arg_this_that,
+                              "that": self.push_lcl_arg_this_that,
+                              "temp": self.push_tmp_pt,
+                              "pointer": self.push_tmp_pt,
+                              "constant": self.push_constant,
+                              "static": self.push_static}
+            self.output_stream.write(push_functions[segment](segment, index))
+        elif command == "C_POP":
+            pop_functions = {"local": self.pop_lcl_arg_this_that,
+                             "argument": self.pop_lcl_arg_this_that,
+                             "this": self.pop_lcl_arg_this_that,
+                             "that": self.pop_lcl_arg_this_that,
+                             "temp": self.pop_tmp_pt,
+                             "pointer": self.pop_tmp_pt,
+                             "static": self.pop_static}
+            self.output_stream.write(pop_functions[segment](segment, index))
+
+    def close(self) -> None:
+        """Closes the output file."""
+        # Your code goes here!
+        self.output_stream.close()
+
+    # arithmetic implementations
+    @staticmethod
+    def add_sub(command: str) -> str:
+        end = "M=D+M\n" if command == "add" else "M=M-D\n"
+        return f"// {command}\n" + CodeWriter.pop_SP + \
+               f"A=A-1\n" + end
+
+    @staticmethod
+    def neg_not_shift(command: str) -> str:
+        operand_dic = {"neg": "-M", "not": "!M",
+                       "shiftleft": "M<<", "shiftright": "M>>"}
+        return f"// {command}\n" \
+               f"@SP\n" \
+               f"A=M-1\n" \
+               f"M={operand_dic[command]}\n"
+
+    def eq(self, command: str) -> str:
+        res = f"// {command}\n" \
+              "@SP\n" \
+              "AM=M-1\n" \
+              "D=M\n" \
+              "M=0\n" \
+              "@R15\n" \
+              "M=D\n" \
+              f"@POS.{self.label_counter}\n" \
+              f"D;JGT\n" \
+              f"@SP\n" \
+              f"A=M-1\n" \
+              f"D=M\n" \
+              f"M=0\n" \
+              f"@CONTINUE.{self.label_counter}\n" \
+              f"D;JGT\n" \
+              f"@REG.{self.label_counter}\n" \
+              f"0;JMP\n" \
+              f"(POS.{self.label_counter})\n" \
+              f"@SP\n" \
+              f"A=M-1\n" \
+              f"D=M\n" \
+              f"M=0\n" \
+              f"@CONTINUE.{self.label_counter}\n" \
+              f"D;JLT\n" \
+              f"(REG.{self.label_counter})\n" \
+              f"@R15\n" \
+              f"D=D-M\n" \
+              f"@CONTINUE.{self.label_counter}\n" \
+              f"D;JLT\n" \
+              f"D;JGT\n" \
+              f"(TRUE.{self.label_counter})\n" \
+              f"@SP\n" \
+              f"A=M-1\n" \
+              f"M=-1\n" \
+              f"(CONTINUE.{self.label_counter})\n"
+        self.label_counter += 1
+        return res
+
+    def gt(self, command: str) -> str:
+        res = f"// {command}\n" \
+              "@SP\n" \
+              "AM=M-1\n" \
+              "D=M\n" \
+              "M=0\n" \
+              "@R15\n" \
+              "M=D\n" \
+              f"@NEG.{self.label_counter}\n" \
+              f"D;JLT\n" \
+              f"@SP\n" \
+              f"A=M-1\n" \
+              f"D=M\n" \
+              f"M=0\n" \
+              f"@CONTINUE.{self.label_counter}\n" \
+              f"D;JLT\n" \
+              f"@REG.{self.label_counter}\n" \
+              f"0;JMP\n" \
+              f"(NEG.{self.label_counter})\n" \
+              f"@SP\n" \
+              f"A=M-1\n" \
+              f"D=M\n" \
+              f"M=0\n" \
+              f"@TRUE.{self.label_counter}\n" \
+              f"D;JGT\n" \
+              f"(REG.{self.label_counter})\n" \
+              f"@R15\n" \
+              f"D=D-M\n" \
+              f"@CONTINUE.{self.label_counter}\n" \
+              f"D;JLE\n" \
+              f"(TRUE.{self.label_counter})\n" \
+              f"@SP\n" \
+              f"A=M-1\n" \
+              f"M=-1\n" \
+              f"(CONTINUE.{self.label_counter})\n"
+        self.label_counter += 1
+        return res
+
+    def lt(self, command: str) -> str:
+        res = f"// {command}\n" \
+              "@SP\n" \
+              "AM=M-1\n" \
+              "D=M\n" \
+              "M=0\n" \
+              "@R15\n" \
+              "M=D\n" \
+              f"@POS.{self.label_counter}\n" \
+              f"D;JGT\n" \
+              f"@SP\n" \
+              f"A=M-1\n" \
+              f"D=M\n" \
+              f"M=0\n" \
+              f"@CONTINUE.{self.label_counter}\n" \
+              f"D;JGT\n" \
+              f"@REG.{self.label_counter}\n" \
+              f"0;JMP\n" \
+              f"(POS.{self.label_counter})\n" \
+              f"@SP\n" \
+              f"A=M-1\n" \
+              f"D=M\n" \
+              f"M=0\n" \
+              f"@TRUE.{self.label_counter}\n" \
+              f"D;JLT\n" \
+              f"(REG.{self.label_counter})\n" \
+              f"@R15\n" \
+              f"D=D-M\n" \
+              f"@CONTINUE.{self.label_counter}\n" \
+              f"D;JGE\n" \
+              f"(TRUE.{self.label_counter})\n" \
+              f"@SP\n" \
+              f"A=M-1\n" \
+              f"M=-1\n" \
+              f"(CONTINUE.{self.label_counter})\n"
+        self.label_counter += 1
+        return res
+
+    @staticmethod
+    def and_or(command: str) -> str:
+        operand = "&" if command == "and" else "|"
+        return f"// {command}\n" \
+               + CodeWriter.pop_SP + \
+               f"A=A-1\n" \
+               f"M=D{operand}M\n"
+
+    # push pop implementations
+    @staticmethod
+    def push_constant(segment: str, index: int) -> str:
+        return f"// push {segment} {index}\n" \
+               f"@{index}\n" \
+               "D=A\n" + CodeWriter.push_SP
+
+    @staticmethod
+    def push_lcl_arg_this_that(segment: str, index: int) -> str:
+        return f"// push {segment} {index}\n" \
+               f"@{index}\n" \
+               "D=A\n" \
+               f"@{CodeWriter.segment_dic[segment]}\n" \
+               "A=D+M\n" \
+               "D=M\n" + CodeWriter.push_SP
+
+    @staticmethod
+    def pop_lcl_arg_this_that(segment: str, index: int) -> str:
+        return f"// pop {segment} {index}\n" \
+               f"@{index}\n" \
+               f"D=A\n" \
+               f"@{CodeWriter.segment_dic[segment]}\n" \
+               f"D=D+M\n" \
+               f"@R15\n" \
+               f"M=D\n" \
+               + CodeWriter.pop_SP + \
+               f"@R15\n" \
+               f"A=M\n" \
+               f"M=D\n"
+
+    @staticmethod
+    def push_tmp_pt(segment: str, index: int) -> str:
+        return f"// push {segment} {index}\n" \
+               f"@{CodeWriter.segment_dic[segment] + index}\n" \
+               "D=M\n" + CodeWriter.push_SP
+
+    @staticmethod
+    def pop_tmp_pt(segment: str, index: int):
+        return f"// pop {segment} {index}\n" + CodeWriter.pop_SP + \
+               f"@{CodeWriter.segment_dic[segment] + index}\n" \
+               f"M=D\n"
+
+    def push_static(self, segment: str, index: int) -> str:
+        return f"// push {segment} {index}\n" \
+               f"@{self.file_name}.{index}\n" \
+               "D=M\n" + CodeWriter.push_SP
+
+    def pop_static(self, segment: str, index: int) -> str:
+        return f"// pop {segment} {index}\n" + CodeWriter.pop_SP + \
+               f"@{self.file_name}.{index}\n" \
+               f"M=D\n"
---
+++ 08/Parser.py
@@ -0,0 +1,105 @@
+"""This file is part of nand2tetris, as taught in The Hebrew University,
+and was written by Aviv Yaish according to the specifications given in  
+https://www.nand2tetris.org (Shimon Schocken and Noam Nisan, 2017)
+and as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0 
+Unported License (https://creativecommons.org/licenses/by-nc-sa/3.0/).
+"""
+import typing
+
+
+class Parser:
+    """
+    Handles the parsing of a single .vm file, and encapsulates access to the
+    input code. It reads VM commands, parses them, and provides convenient 
+    access to their components. 
+    In addition, it removes all white space and comments.
+    """
+
+    def __init__(self, input_file: typing.TextIO) -> None:
+        """Gets ready to parse the input file.
+
+        Args:
+            input_file (typing.TextIO): input file.
+        """
+        # Your code goes here!
+        # A good place to start is:
+        input_lines = input_file.read().splitlines()
+        new_lines = list()
+        for line in input_lines:
+            line = line.replace("\t", "")
+            line = line.split(" ")
+            if "//" in line[0]:
+                continue
+            if not line[0]:
+                continue
+            new_line = list()
+            for arg in line:
+                if arg and "//" not in arg:
+                    new_line.append(arg)
+            new_lines.append(new_line)
+
+        self.lines = new_lines
+        self.end_line = len(new_lines)
+        self.line_counter = 0
+        self.cur_command = self.lines[self.line_counter]
+
+    def has_more_commands(self) -> bool:
+        """Are there more commands in the input?
+
+        Returns:
+            bool: True if there are more commands, False otherwise.
+        """
+        # Your code goes here!
+        return bool(self.line_counter < self.end_line)
+
+    def advance(self) -> None:
+        """Reads the next command from the input and makes it the current 
+        command. Should be called only if has_more_commands() is true. Initially
+        there is no current command.
+        """
+        # Your code goes here!
+        self.line_counter += 1
+        if self.has_more_commands():
+            self.cur_command = self.lines[self.line_counter]
+
+    def command_type(self) -> str:
+        """
+        Returns:
+            str: the type of the current VM command.
+            "C_ARITHMETIC" is returned for all arithmetic commands.
+            For other commands, can return:
+            "C_PUSH", "C_POP", "C_LABEL", "C_GOTO", "C_IF", "C_FUNCTION",
+            "C_RETURN", "C_CALL".
+        """
+        # Your code goes here!
+        command_length = len(self.cur_command)
+        command = self.cur_command[0]
+        if command_length == 1:
+            return "C_ARITHMETIC"
+        if command_length == 3:
+            if command == "pop":
+                return "C_POP"
+            if command == "push":
+                return "C_PUSH"
+
+    def arg1(self) -> str:
+        """
+        Returns:
+            str: the first argument of the current command. In case of 
+            "C_ARITHMETIC", the command itself (add, sub, etc.) is returned. 
+            Should not be called if the current command is "C_RETURN".
+        """
+        # Your code goes here!
+
+        return self.cur_command[0] if self.command_type() == "C_ARITHMETIC" \
+            else self.cur_command[1]
+
+    def arg2(self) -> int:
+        """
+        Returns:
+            int: the second argument of the current command. Should be
+            called only if the current command is "C_PUSH", "C_POP", 
+            "C_FUNCTION" or "C_CALL".
+        """
+        # Your code goes here!
+        return int(self.cur_command[2])
